6bbb9a0f075906fd257dba6576f3e8f9c4b6a687
Ante-237 <akumawahanye@gmail.com>
2022-06-27 12:10:39 +0200

    test

--- python-hello_world/9-easter_egg.py
+++ python-hello_world/9-easter_egg.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python3
-print("""\
+f='is better than'
+s='If the implementation is'
+print(f"""\
 The Zen of Python, by Time Peters
 
 
-Beautiful is better than ugly.
-Explicit is better than implicit.
-Simple is better than complex.
-Complex is better than complicated.
-Flat is better than nested.
-Sparse is better than dense.
+Beautiful {f} ugly.
+Explicit {f} implicit.
+Simple {f} complex.
+Complex {f} complicated.
+Flat {f} nested.
+Sparse {f} dense.
 Readability counts.
 Special cases aren't special enough to break the rules.
 Although practicality beats purity.
@@ -17,9 +19,9 @@ Unless explicitly silenced.
 In the face of ambiguity, refuse the temptation to guess.
 There should be one-- and preferably only one --obvious way to do it.
 Although that way may not be obvious at first unless you're Dutch.
-Now is better than never.
+Now {f} never.
 Although never is often better than *right* now.
-If the implementation is hard to explain, it's a bad idea.
-If the implementation is easy to explain, it may be a good idea.
+{s} hard to explain, it's a bad idea.
+{s} easy to explain, it may be a good idea.
 Namespaces are one honking great idea -- let's do more of those!
 """)
