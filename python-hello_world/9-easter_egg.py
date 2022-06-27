f25d73298ca63a29fd6457747cd24a6adee53966
Ante-237 <akumawahanye@gmail.com>
2022-06-27 21:05:04 +0200

    fix

--- python-hello_world/9-easter_egg.py
+++ python-hello_world/9-easter_egg.py
@@ -1,28 +1,2 @@
 #!/usr/bin/python3
-f='is better than'
-s='If the implementation is'
-print(f"""\
-The Zen of Python, by Time Peters
-
-
-Beautiful {f} ugly.
-Explicit {f} implicit.
-Simple {f} complex.
-Complex {f} complicated.
-Flat {f} nested.
-Sparse {f} dense.
-Readability counts.
-Special cases aren't special enough to break the rules.
-Although practicality beats purity.
-Errors should never pass silently.
-Unless explicitly silenced.
-In the face of ambiguity, refuse the temptation to guess.
-There should be one-- and preferably only one --obvious way to do it.
-Although that way may not be obvious at first unles
-+s you're Dutch.
-Now {f} never.
-Although never is often f{3:} *right* now.
-{s} hard to explain, it's a bad idea.
-{s} easy to explain, it may be a good idea.
-Namespaces are one honking great idea -- let's do more of those!
-""")
+print("The Zen of Python, by TimePeters")
