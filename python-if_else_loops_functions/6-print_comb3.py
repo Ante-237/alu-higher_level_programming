31cdb2686c0ebba591543ce2dcf9e7b6e3c5d022
Ante-237 <akumawahanye@gmail.com>
2022-06-28 16:20:33 +0200

    (:) fix

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -3,5 +3,5 @@ for i in range(0, 8):
     for j in range(0, 10):
         if j <= i: 
             continue
-        print("{}{}".format(i,j), end=", ")
+        print("{}{}".format(i, j), end=", ")
 print("89")
