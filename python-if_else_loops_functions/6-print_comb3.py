9526965dac0e291ea504f6251f4a763b58791fe3
Ante-237 <akumawahanye@gmail.com>
2022-06-28 16:42:25 +0200

    change of code style :))

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-for i in range(0, 8):
-    for j in range(0, 10):
-        if j<=i: 
-            continue
-        print("{}{}".format(i, j), end=", ")
-print("89")
+for i in range(0, 90):
+    if i != 89:
+        if i % 10 > i / 10:
+            print("{:02d}".format(i),end=", ")
+    else:
+        print("{:d}".format(i))
