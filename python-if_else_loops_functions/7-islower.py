e0843cadce19f18cbf9a608de5e3ba0731617682
Ante-237 <akumawahanye@gmail.com>
2022-06-28 17:05:58 +0200

    0\_/0

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -2,6 +2,8 @@
 def is_lower(c):
     value = ord(c)
     if value >= 97 and value <= 122:
-        return true
-    else:
-        return false
+        return True
+    elif value >= 48 and value <= 57:
+        return False
+    elif value >= 65 and value <= 90:
+        return False
