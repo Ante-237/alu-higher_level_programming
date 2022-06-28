eac2db7ab140803f267b4173d224a99dabfeba2c
Ante-237 <akumawahanye@gmail.com>
2022-06-28 16:56:37 +0200

    fix :))

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def is_lower(c):
     value = ord(c)
-    if value >= 65 and value <= 90:
-        return false
-    else:
+    if value >= 97 and value <= 122:
         return true
+    else:
+        return false
