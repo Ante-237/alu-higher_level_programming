113c5787127b26952757bc0a6304e3791a17d980
Ante-237 <akumawahanye@gmail.com>
2022-06-30 08:57:37 +0200

    last fix o\_/o

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -6,13 +6,13 @@ if __name__ == "__main__":
 
 argNum = len(sys.argv)  # gets number of arguments
 allA = sys.argv  # stores arguments return by sys module
-a = int(allA[1])
-b = int(allA[3])
-s = allA[2]
 if argNum != 4:
     print("Usage: ./100-my_calculator.py <a> <operator> <b>")
     exit(1)
 else:
+    a = int(allA[1])
+    b = int(allA[3])
+    s = int(allA[2])
     if s == "-":
         print("{:d} - {:d} = {:d}".format(a, b, extra.sub(a, b)))
     elif s == "*":
