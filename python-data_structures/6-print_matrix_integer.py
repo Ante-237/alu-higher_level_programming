3fac8fcd9d95589332bb6b5a515e469a195692fb
Ante-237 <akumawahanye@gmail.com>
2022-07-11 23:38:41 +0200

    change fix

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
+    counter = 1
     for record in matrix:
         for i in record:
-            print("{:d}".format(i), end=" ")
+            if counter%3 == 0:
+                print("{:d}".format(i), end="")
+            else:
+                print("{:d}".format(i), end=" ")
+            counter += 1
         print("")
