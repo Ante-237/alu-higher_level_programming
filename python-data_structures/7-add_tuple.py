b3819c39eb220fc63a7044e90d04889374971229
Ante-237 <akumawahanye@gmail.com>
2022-07-12 01:01:45 +0200

    change :) fix

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -10,11 +10,6 @@ def add_tuple(tuple_a=(), tuple_b=()):
         sum_t.append(tuple_a[1] + tuple_b[1])
         x = tuple(sum_t)
         return x
-    if a_len < 2 and b_len < 2:
-        sum_t.append(tuple_a[0] + tuple_b[0])
-        sum_t.append(0 + 0)
-        x = tuple(sum_t)
-        return x
     if a_len < 2 or b_len < 2:
         if a_len < 1:
             y = list(tuple_a)
