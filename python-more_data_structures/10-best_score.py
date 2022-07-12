7bb8205b1b0661294afc099e9323a8a84e359507
Ante-237 <akumawahanye@gmail.com>
2022-07-12 22:44:17 +0200

    o\\\\_/////o

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -2,5 +2,10 @@
 
 
 def best_score(a_dictionary):
-    list_me = list(sorted(a_dictionary, key=lambda x : x[1], reverse=True))
-    return list_me[0]
+    if len(a_dictionary) <= 0:
+        return "None"
+    elif len(a_dictionary) > 0:
+        list_me = dict(sorted(a_dictionary.items(), key=lambda x: x[1], reverse=True))
+        return list_me.keys()[0]
+    else:
+        return "None"
