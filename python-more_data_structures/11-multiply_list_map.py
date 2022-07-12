82cbf4b908785224039b72a432d78075593f51e4
Ante-237 <akumawahanye@gmail.com>
2022-07-12 22:57:46 +0200

    fix

--- python-more_data_structures/11-multiply_list_map.py
+++ python-more_data_structures/11-multiply_list_map.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-
-
 def multiply_list_map(my_list=[], number=0):
-    the_list = list(map(my_list, key=lambda x: x * number))
-    return the_list
+    return list(map(my_list, key=lambda x: x * number))
+    
