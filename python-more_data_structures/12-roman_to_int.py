cdd78c78f27f7df26c855df46feac307857f2833
Ante-237 <akumawahanye@gmail.com>
2022-07-12 23:29:20 +0200

    fix

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -3,22 +3,31 @@
 
 def roman_to_int(roman_string):
     final_value = 0
-    if type(roman_string) != type("") or roman_string is None:
+    counter = 0
+    if isinstance(roman_string, str) or roman_string is None:
         return final_value
     else:
         for i in roman_string:
             if i == "M":
                 final_value += 1000
+                counter += 1
             elif i == "D":
                 final_value += 500
+                counter += 1
             elif i == "C":
                 final_value += 100
+                counter += 1
             elif i == "L":
                 final_value += 50
+                counter += 1
             elif i == "X":
                 final_value += 10
+                counter += 1
             elif i == "V":
                 final_value += 5
-            elif i == "I":
+                counter += 1
+            elif i == "I" and counter == 0:
                 final_value += 1
+            elif i == "I" and counter > 0:
+                final_value -= 1
         return final_value
