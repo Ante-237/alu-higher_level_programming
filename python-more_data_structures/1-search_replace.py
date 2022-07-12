56f6f6790c748ddbe1f3f9507c8b62df23738dbb
Ante-237 <akumawahanye@gmail.com>
2022-07-12 20:09:38 +0200

    changes fix :)

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -2,7 +2,6 @@
 
 
 def search_replace(my_list, search, replace):
-    for i in range(0,len(my_list)):
+    for i in range(0, len(my_list)):
         if search == my_list[i]:
             my_list[i] = replace
-    return my_list
