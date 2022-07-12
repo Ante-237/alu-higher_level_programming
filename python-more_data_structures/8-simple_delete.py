1fd75f744c4a80ae8a74a85b5c5a0096211c29f0
Ante-237 <akumawahanye@gmail.com>
2022-07-12 22:07:59 +0200

    fix

--- python-more_data_structures/8-simple_delete.py
+++ python-more_data_structures/8-simple_delete.py
@@ -4,7 +4,7 @@
 
 def simplel_delete(a_dictionary, key=""):
     if key == "":
-        return a_dictionary:
+        return a_dictionary
     else:
         del a_dictionary[key]
         return a_dictionary
