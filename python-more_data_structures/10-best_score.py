17306fe288a012bd0a8530e0da39ca705fdf265b
Ante-237 <akumawahanye@gmail.com>
2022-07-12 22:46:23 +0200

    fix -

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -5,7 +5,8 @@ def best_score(a_dictionary):
     if len(a_dictionary) <= 0:
         return "None"
     elif len(a_dictionary) > 0:
-        list_me = dict(sorted(a_dictionary.items(), key=lambda x: x[1], reverse=True))
-        return list_me.keys()[0]
+        dict_me = dict(sorted(a_dictionary.items(), key=lambda x: x[1], reverse=True))
+        list_me = list(dict_me)
+        return list_me[0]
     else:
         return "None"
