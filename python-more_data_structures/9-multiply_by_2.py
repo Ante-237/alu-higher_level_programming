aac4a702f4c18efc9f9058a3e7b095954d920fe3
Ante-237 <akumawahanye@gmail.com>
2022-07-12 22:21:18 +0200

    fix :)

--- python-more_data_structures/9-multiply_by_2.py
+++ python-more_data_structures/9-multiply_by_2.py
@@ -3,6 +3,6 @@
 
 def multiply_by_2(a_dictionary):
     temp_dic = {}
-    for key,val in a_dictionary.items():
+    for key, val in a_dictionary.items():
         temp_dic[key] = val * 2
     return temp_dic
