8014a4a23db9d00d0114766b30b25c0fef61c6eb
Ante-237 <akumawahanye@gmail.com>
2022-07-14 19:21:23 +0200

    fix one

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -8,11 +8,11 @@ def roman_to_int(roman_string):
     total = 0
     num = 0
     digits = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    # reverese the roman numerals to start with the tens of units and use each unit
+    # reveresetheromannumeralstostartwiththetensofunitsanduseeach unit
     for r in reversed(roman_string):
         num = digits[r]
         if total < num * 5:
             total += num
         else:
-            num -= 1
+            -num
     return total
