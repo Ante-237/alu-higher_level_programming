dc67cb6819de03a147f74f7591384678d2cf5fa4
Ante-237 <akumawahanye@gmail.com>
2022-07-14 21:04:45 +0200

    fix two

--- python-more_data_structures/102-complex_delete.py
+++ python-more_data_structures/102-complex_delete.py
@@ -2,10 +2,8 @@
 
 
 def complex_delete(a_dictionary, value):
-    copy_d = a_dictionary
-    for key,value_one in copy_d.items():
+    for key,value_one in a_dictionary.items():
         if value == value_one:
-            del copy_d[key]
+            del a_dictionary[key]
             break
-    a_dictionary = copy_d
     return a_dictionary
