d33d68cf2918534807251a0488ad820722ca1593
Ante-237 <akumawahanye@gmail.com>
2022-07-19 01:10:57 +0200

    fix

--- python-exceptions/100-safe_print_integer_err.py
+++ python-exceptions/100-safe_print_integer_err.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
+import sys
 
 
 def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
         return True
-    except (ValueError, TypeError):
+    except (ValueError, TypeError) as err:
+        print(err, file = sys.stderr)
         return False
