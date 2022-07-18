f630b6f3a21be062b7c138046d7c020f53225692
Ante-237 <akumawahanye@gmail.com>
2022-07-18 21:36:20 +0200

    some change

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -4,8 +4,8 @@
 def safe_print_list(my_list=[], x=0):
     count = 0
     try:
-        for i in range(0, x):
-            print("{:d}".format(my_list[i]), end="")
+        while count < 0:
+            print("{:d}".format(my_list[count]), end="")
             count += 1
     except:
         pass
