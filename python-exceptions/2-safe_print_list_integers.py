3995744e7a357665a3eae6e1d1f25d9910adb18f
Ante-237 <akumawahanye@gmail.com>
2022-07-18 22:57:41 +0200

    lastfix

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -5,7 +5,7 @@ def safe_print_list_integers(my_list=[], x=0):
     count = 0
     num = 0
     while count < x:
-        try:    
+        try:
             print("{:d}".format(my_list[count]), end="")
             num += 1
         except (TypeError, ValueError):
