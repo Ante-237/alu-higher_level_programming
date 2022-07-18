9363f118268075aa6da59869b3f25b86644571a6
Ante-237 <akumawahanye@gmail.com>
2022-07-18 21:49:01 +0200

    fix :)

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -4,10 +4,10 @@
 def safe_print_list(my_list=[], x=0):
     count = 0
     try:
-        while count < 0:
+        while count < x:
             print(my_list[count], end="")
             count += 1
     except IndexError:
         pass
     finally:
-        print("\n",my_list[count])
+        print("\n", my_list[count])
