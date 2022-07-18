adec39ecba52865ffd2a79c18d380c3362deea7b
Ante-237 <akumawahanye@gmail.com>
2022-07-18 20:51:58 +0200

    show fix

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -6,6 +6,7 @@ def safe_print_list(my_list=[], x=0):
         for i in range(0, x):
             print("{}".format(my_list[i]), end="")
     except IndexError:
+        print("")
             
     finally:
         print("")
