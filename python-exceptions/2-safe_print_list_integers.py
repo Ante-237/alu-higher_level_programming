94024370861312440648e03020c9b0fc966690cf
Ante-237 <akumawahanye@gmail.com>
2022-07-18 22:16:36 +0200

    fix

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -9,7 +9,6 @@ def safe_print_list_integers(my_list=[], x=0):
                 print("{:d}".format(my_list[count]), end= "")
                 count += 1
             except:
-                count += 1
                 pass
     except IndexError:
         pass
