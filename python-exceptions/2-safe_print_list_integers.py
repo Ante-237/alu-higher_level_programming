39e5d3a013446fbb473504e81d50956ee80003f9
Ante-237 <akumawahanye@gmail.com>
2022-07-18 22:32:50 +0200

    change fix

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -8,10 +8,9 @@ def safe_print_list_integers(my_list=[], x=0):
             try:
                 print("{:d}".format(my_list[count]), end="")
                 count += 1
-            except TypeError:
-                count += 1
-            except ValueError:
-                count += 1
+            except (TypeError, ValueError):
+                pass
+            count += 1
     except IndexError:
         pass
     finally:
