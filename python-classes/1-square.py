db031b118b2514c66e87ba0fdd9cc12025b41c7a
Ante-237 <akumawahanye@gmail.com>
2022-07-19 18:28:44 +0200

    fix sure

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,17 +1,12 @@
 #!/usr/bin/python3
-""" 
+"""
 DOCUMENTATION FOR MODULES IN HERE WILL DO THE TRICK
 """
 
 
-class Square():
+class Square:
     """
     CLASS CREATES A PRIVATE SIZE INSTANCE VARIABLE AND ASSIGNS IT
     """
-
-
-    def __init__(self, size):
-        self.size = size
-        """
-        FUNCTION ASSINGS SIZE TO THE CURRENT INSTANCE
-        """
+    def __init__(self, sizee):
+        self.size = sizee
