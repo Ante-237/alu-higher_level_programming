d23a2b69c3cf8a42dbd9a31d470fe13cf1f5c830
Ante-237 <akumawahanye@gmail.com>
2022-07-19 19:49:16 +0200

    fix

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -9,15 +9,9 @@ class Square:
     SQUARE CLASS
     """
     def __init__(self, size=0):
-        """
-        INSTANCE INIT
-        """
         self._size = size
 
     def size(self, value):
-        """
-        A SETTER
-        """
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
         elif value < 0:
@@ -26,13 +20,7 @@ class Square:
             self._size = value
 
     def size(self):
-        """
-        A GETTER
-        """
-            return self._size
+        return self._size
 
     def area(self):
-        """
-        CALCULATE AREA
-        """
         return self._size**2
