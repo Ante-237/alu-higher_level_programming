a4cc6fb0c305e475b8812085e4261b6dad592cdb
anTe <77624934+Ante-237@users.noreply.github.com>
2022-07-19 11:50:20 -0700

    fix sure o\_/o

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -1,53 +1,53 @@
 #!/usr/bin/python3
-""" Module Square """
+""" Module Sqaure """
 
 
 class Square:
-    """
-    SQUARE CLASS
+    """ Square class defined by geometric shap
+        Attributes:
+            size (int): Size of square
     """
     def __init__(self, size=0):
-        """
+        """initializes the square
+        Args:
+            size (int): size of a side of the square
         Returns:
-            just init values
-        Args: 
-            size (int): default to 0 sets side of square
+            None
         """
         self.__size = size
+
+    def area(self):
+        """
+        set square square area
+        Return:
+            the current square area (int)
+        """
+        return self.__size ** 2
+
+    @property
+    def size(self):
+        """
+        getter of size
+        Return:
+            Size of square
+        """
+        return self.__size
 
     @size.setter
     def size(self, value):
         """
-        setter method
+        Setter of size
         Args:
-            value(int): size of the side of the square
-        Raises:
-            TypeError: if not int
-            ValueError: if less than 0
+            size (int): size of a side of the square
+        Raises
+            TypeError: if size is not int
+            ValueError: size less than 0
         Returns:
-            Setter no return
+            None
         """
-        if not isinstance(value, int):
+        if type(value) is not int:
             raise TypeError("size must be an integer")
         elif value < 0:
-            raise ValueError("size must be >= 0")
+                raise ValueError("size must be >= 0")
         else:
             self.__size = value
-    
-    @property
-    def size(self):
-        """
-        GETTER METHOD
-        Return:
-            size of square
-        """
-        return self.__size
-
-    def area(self):
-        """
-        SOME AREA METHOD
-
-        Return:
-            the calculated square area
-        """
-        return self.__size**2
