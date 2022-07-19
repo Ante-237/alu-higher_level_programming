7879e70bd1becfe6cf1e195f1bb220b3a0254aad
Ante-237 <akumawahanye@gmail.com>
2022-07-19 18:12:52 +0200

    init :)

---
+++ python-classes/1-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+""" 
+NO MODULE USED
+"""
+
+
+class Square():
+    """
+    CLASS CREATES A PRIVATE SIZE INSTANCE VARIABLE AND ASSIGNS IT
+    """
+
+
+    __init__(self, size):
+        self.size = size
+        """
+        FUNCTION ASSINGS SIZE TO THE CURRENT INSTANCE
+        """
