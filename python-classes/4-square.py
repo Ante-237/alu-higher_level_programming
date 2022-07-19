99ca1a6ccd4dd80000d3a24dfdf2861441428a93
Ante-237 <akumawahanye@gmail.com>
2022-07-19 19:51:07 +0200

    fix

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -9,7 +9,7 @@ class Square:
     SQUARE CLASS
     """
     def __init__(self, size=0):
-        self._size = size
+        self.__size = size
 
     def size(self, value):
         if not isinstance(value, int):
@@ -17,10 +17,10 @@ class Square:
         elif value < 0:
             raise ValueError("size must be >= 0")
         else:
-            self._size = value
+            self.__size = value
 
     def size(self):
-        return self._size
+        return self.__size
 
     def area(self):
-        return self._size**2
+        return self.__size**2
