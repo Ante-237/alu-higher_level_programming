65c9790d56fa7beda3507cdedd58eff8f7dea083
Ante-237 <akumawahanye@gmail.com>
2022-07-26 16:12:25 +0200

    fix

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -6,10 +6,10 @@ Rectangle = __import__("9-rectangle.py").Rectangle
 
 
 class Square(Rectangle):
-    """ square class """
+    """ square class that makes use of parent class """
 
     def __init__(self, size):
-        """ class init """
+        """ class init which class parent init """
         self.integer_validator("size", size)
         self.__size = size
         super().__init_(size, size)
