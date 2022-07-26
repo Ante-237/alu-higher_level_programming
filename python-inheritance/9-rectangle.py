d8f7d96fec15e937a666cb27814b263d805eac36
Ante-237 <akumawahanye@gmail.com>
2022-07-26 16:01:26 +0200

    fix sure

--- python-inheritance/9-rectangle.py
+++ python-inheritance/9-rectangle.py
@@ -21,4 +21,4 @@ class Rectangle(BaseGeometry):
 
     def __str__(self):
         """ area string represent """
-        return ("[Rectangle]  {:d}/{:d}".format(self.__width, self.__height))
+        return ("[Rectangle] {:d}/{:d}".format(self.__width, self.__height))
