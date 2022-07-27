31c4430d5fb2bd9406658b4e195fc55cbb17266e
Ante-237 <akumawahanye@gmail.com>
2022-07-27 15:03:13 +0200

    multi test drive

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -8,6 +8,6 @@ import sys
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-
-save_to_json_file(sys.argv[1:], "add_item.json")
+new_list = new_list + argv[1:]
+save_to_json_file(new_list, "add_item.json")
 load_from_json_file("add_item.json")
