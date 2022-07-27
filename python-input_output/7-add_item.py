ed3b5eedc2f11586425017f74ab86e3b96ec8b03
Ante-237 <akumawahanye@gmail.com>
2022-07-27 14:10:56 +0200

    fix doc

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,14 +1,9 @@
 #!/usr/bin/python3
-""" importing json modulel """
-import json
-""" using sys """
+""" importing json module and those argv manupulation from sys module  """
 import sys
-""" using other external modules """
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file.py').load_from_json_file
-""" module uses mostly external modules """
 
 
-""" calling functions to write and load from json file """
 save_to_json_file(sys.argv,"add_item.json")
 load_from_json_file("add_item.json")
