d5b2e1e1bb1b1809a0ea695ddbf2ab4ec3e7b3aa
Ante-237 <akumawahanye@gmail.com>
2022-07-27 12:53:20 +0200

    fix pep8 issue

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ module for opening file """
 
+
 def read_file(filename=""):
     """ funciton opens and read file passed by name """
     with open(filename, encoding="utf-8") as f:
