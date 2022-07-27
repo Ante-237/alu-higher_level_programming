d00c06dc6cfd991bc7d62afd7e9d067f0fa0e0e6
Ante-237 <akumawahanye@gmail.com>
2022-07-27 15:48:12 +0200

    fix pep8

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -5,7 +5,6 @@
 class Student:
     """ empty class """
 
-
     def __init__(self, first_name, last_name, age):
         """ just init public instance attributes """
         self.first_name = first_name
