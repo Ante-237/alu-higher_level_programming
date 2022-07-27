6427b0b5935bbdf2227a51d2669076a7153f394f
anTe <77624934+Ante-237@users.noreply.github.com>
2022-07-27 07:42:19 -0700

    update fix

--- python-input_output/11-student.py
+++ python-input_output/11-student.py
@@ -1,23 +1,32 @@
 #!/usr/bin/python3
-""" student to json filter """
+"""
+A Student class that defines a Studen (module)
+"""
 
 
-class Student:
-    """ this is a student class """
-
+class Student():
+    """
+        A Student class that defines a Student
+    """
     def __init__(self, first_name, last_name, age):
-        """ just doing init for variables """
+        """
+            INIT
+        """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        """ method converts to json """
+        """
+            retrieves a dictionary representation
+            of a Student instance
+        """
         if type(attrs) is list and all([type(x) == str for x in attrs]):
             return {k: v for k, v in self.__dict__.items() if k in attrs}
-        return self.__dict__
+        return(self.__dict__)
 
     def reload_from_json(self, json):
-        """ return json """
-        for i,j in json.items():
-            self.__dict__(i) = j
+        """
+        """
+        for i, j in json.items():
+            self.__dict__[i] = j
