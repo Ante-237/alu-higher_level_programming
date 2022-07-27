7ce94fd8b6dd838529c42f7749361c5733af5a8f
Ante-237 <akumawahanye@gmail.com>
2022-07-27 14:06:57 +0200

    documentation fix

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 """ importing json modulel """
 import json
+""" using sys """
 import sys
+""" using other external modules """
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file.py').load_from_json_file
 """ module uses mostly external modules """
