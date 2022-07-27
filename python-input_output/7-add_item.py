598f29a23091336fcb02368c18e949f2882468c2
Ante-237 <akumawahanye@gmail.com>
2022-07-27 14:03:18 +0200

    init first

---
+++ python-input_output/7-add_item.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" importing json modulel """
+import json
+import sys
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file.py').load_from_json_file
+""" module uses mostly external modules """
+
+
+""" calling functions to write and load from json file """
+save_to_json_file(sys.argv,"add_item.json")
+load_from_json_file("add_item.json")
