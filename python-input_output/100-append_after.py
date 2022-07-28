8c47fe94f358fec52f3c459c9e851b66e1ecafd1
anTe <77624934+Ante-237@users.noreply.github.com>
2022-07-28 14:05:41 -0700

    sure :) update

--- python-input_output/100-append_after.py
+++ python-input_output/100-append_after.py
@@ -3,3 +3,17 @@
 open find replace close
 """
 
+
+def append_after(filename="", search_string="", new_string=""):
+    """
+    inserts a line of text to a file,
+    after each line containing a specific string
+    """
+    with open(filename, "r+", encoding="utf-8") as f:
+        s = ""
+        for line in f:
+            s += line
+            if search_string in line:
+                s += new_string
+        f.seek(0)
+        f.write(s)
