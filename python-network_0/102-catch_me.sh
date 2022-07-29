83dae6f88bdbe06d25f60feef075ed7912db07fc
Ante-237 <akumawahanye@gmail.com>
2022-07-29 13:53:47 +0200

    change linked to

--- python-network_0/102-catch_me.sh
+++ python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # bash script makes request to certain ip , ctf style 
-curl -sL 0.0.0.0:5000/catch_me -X PUT "You got me!" -H "Origin: HolbertonSchool" -d "user_id=98".
+curl -sL 0.0.0.0:5000/catch_me -X PUT "You got me!" -H "Origin: School" -d "user_id=98".
