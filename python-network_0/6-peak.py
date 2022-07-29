89b7e532381c8aa3bd4c212bf370c8f18a0ca70d
anTe <77624934+Ante-237@users.noreply.github.com>
2022-07-29 04:13:33 -0700

    update fix

--- python-network_0/6-peak.py
+++ python-network_0/6-peak.py
@@ -1,20 +1,22 @@
 #!/usr/bin/python3
 """
-function  uses the famous binary sort 
-to sort integers 
+function that finds a peak in a list of unsorted
+integers using binary sort
 """
 
 
 def find_peak(list_of_integers):
     """
-    find peak using binary sort
+    function to find peak
     """
     l = len(list_of_integers)
     if l == 0:
         return
     half = l // 2
-    if (half == l - 1 or list_of_integers[half] >= list_of_integers[half + 1] and (half == 0 or list_of_integers[half] >= list_of_integers[half - 1]):
-            return (list_of_integers[half])
+    if (half == l - 1 or list_of_integers[half] >=
+        list_of_integers[half + 1]) and (half == 0 or list_of_integers[half] >=
+                                         list_of_integers[half - 1]):
+        return (list_of_integers[half])
     elif half != l - 1 and list_of_integers[half + 1] > list_of_integers[half]:
-            return (find_peak(list_of_integers[half + 1:]))
+        return (find_peak(list_of_integers[half + 1:]))
     return (find_peak(list_of_integers[:half]))
