4ae848d0ec96dee1095c54deb172ebef9f42ca81
Ante-237 <akumawahanye@gmail.com>
2022-07-29 13:09:56 +0200

    using best sort

---
+++ python-network_0/6-peak.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+module uses the famous binary sort 
+to sort integers
+"""
+
+
+def find_peak(list_of_integers):
+    """
+    find peak using binary sort
+    """
+    l = len(list_of_integers)
+    if l == 0:
+        return
+    half = l // 2
+    if (half == l - 1 or list_of_integers[half] >= list_of_integers[half + 1] and (half == 0 or list_of_integers[half] >= list_of_integers[half - 1]):
+            return (list_of_integers[half])
+    elif half != l - 1 and list_of_integers[half + 1] > list_of_integers[half]:
+            return (find_peak(list_of_integers[half + 1:]))
+    return (find_peak(list_of_integers[:half]))
