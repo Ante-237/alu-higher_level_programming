d4bc2da51d04216e47e8d11c8673dbddcbe2a6f4
Ante-237 <akumawahanye@gmail.com>
2022-08-01 20:54:07 +0200

    fix pep8

--- python-network_1/2-post_email.py
+++ python-network_1/2-post_email.py
@@ -8,9 +8,9 @@ import urllib.parse
 
 if __name__ == '__main__':
     url = sys.argv[1]
-    values = {'email' :  sys.argv[2]}
+    values = {'email': sys.argv[2]}
     data = urllib.parse.urlencode(values)
-    data = data.encode('ascii') 
+    data = data.encode('ascii')
     req = urllib.request.Request(url, data)
     with urllib.request.urlopen(req) as response:
         the_page = response.read()
