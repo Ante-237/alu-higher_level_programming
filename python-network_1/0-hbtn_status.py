36d020e1b918501be0bcefc1551b8916fdf08ccb
Ante-237 <akumawahanye@gmail.com>
2022-08-01 19:16:06 +0200

    using tabs

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
     with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
         html = response.read()
         print('Body response:')
-        print("         - type: {}".format(type(html)))
-        print("         - content: {}".format(html))
+        print("\t- type: {}".format(type(html)))
+        print("\t- content: {}".format(html))
         temp = str(html)
-        print("         - utf8 content: {}".format(temp[2:-1]))
+        print("\t- utf8 content: {}".format(temp[2:-1]))
