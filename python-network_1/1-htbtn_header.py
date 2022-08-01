04c19c1d25eabde2d6ffbf7adcb4ab022e058787
Ante-237 <akumawahanye@gmail.com>
2022-08-01 20:12:23 +0200

    init response header value #0

---
+++ python-network_1/1-htbtn_header.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" module uses another module to request to make request """
+import urllib.request
+import sys 
+
+"""making request to provided url"""
+if __name__ == "__main__":
+    """making request to provided url"""
+    req = sys.argv[1]
+    with urllib.request.urlopen(req) as response:
+        print(response.headers.get('X-Request-Id'))
