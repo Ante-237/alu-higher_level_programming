3749bea81e8b9e983a67324eed8406cdf680532a
Ante-237 <akumawahanye@gmail.com>
2022-08-03 13:25:30 +0200

    fix

--- python-network_1/10-my_github.py
+++ python-network_1/10-my_github.py
@@ -6,10 +6,10 @@ import sys
 import requests
 
 
-user_name = "Ante-237"
+url = 'https://api.github.com/user'
 
 if __name__ == "__main__":
-    res = requests.get('https://api.github.com/user',auth=(sys.argv[1], sys.argv[2]))
+    res = requests.get(url, auth=(sys.argv[1], sys.argv[2])).json()
     if 'id' in res:
         print(res['id'])
     else:
