ab86e949b24b535232345d39e59f96a585a775ac
Ante-237 <akumawahanye@gmail.com>
2022-08-03 13:41:55 +0200

    init

---
+++ python-network_1/100-github_commits.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" api request
+    github"""
+import sys
+import requests
+
+url = "https://api.github.com/repos"
+
+if __name__ == "__main__":
+    res = requests.get('https://api.github.com/repos/{}/{}/commits'
+                       .format(sys.argv[2], sys.argv[1])).json()
+    print(res)
+    count = 0
+    for commit in res:
+        name = commit.get("commit").get("author").get("name")
+        sha = commit.get("sha")
+        print("{}: {}".format(sha, name))
+        count += 1
+        if count == 10:
+            break
