8c7cb12b7340f0ee1dac5ead634f281b3f36ed91
anTe <77624934+Ante-237@users.noreply.github.com>
2022-08-03 06:59:43 -0700

    update

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,12 +1,9 @@
 -- full creation
-CREATE TABLE IF NOT EXISTS `second_table` (
-	`id` INT,
-	`name` VARCHAR(256),
-	`score` INT
-);
-
-INSERT INTO `second_tables` VALUES
-VALUES ('1', "John", '10'),
-VALUES ('2', "Alex", '3'),
-VALUES ('3', "Bob", '14'),
-VALUES ('4', "George", '8');
+CREATE TABLE IF NOT EXISTS second_table (id INT,
+name VARCHAR(256),
+score INT);
+INSERT INTO second_table (id, name, score)
+VALUES (1, "John", 10),
+(2, "Alex", 3),
+(3, "Bob", 14),
+(4, "George", 8);
