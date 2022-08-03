ce6885ffd8b39603be28ae96841425c040035b1f
Ante-237 <akumawahanye@gmail.com>
2022-08-03 13:22:21 +0200

    to github

--- python-network_1/10-my_github.py
+++ python-network_1/10-my_github.py
@@ -6,5 +6,11 @@ import sys
 import requests
 
 
+user_name = "Ante-237"
+
 if __name__ == "__main__":
-    res = requests.get('
+    res = requests.get('https://api.github.com/user',auth=(sys.argv[1], sys.argv[2]))
+    if 'id' in res:
+        print(res['id'])
+    else:
+        print(None)
