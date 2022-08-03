7a49216edea9f3cebcd4fd329a90fb12fb63d5ef
Ante-237 <akumawahanye@gmail.com>
2022-08-03 15:54:05 +0200

    sure

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,12 +1,12 @@
 -- full creation
-CREATE TABLE IF NOT EXISTS second_table (
-	id INT,
-	name VARCHAR(256),
-	score INT
+CREATE TABLE IF NOT EXISTS `second_table` (
+	`id` INT,
+	`name` VARCHAR(256),
+	`score` INT
 );
 
-INSERT INTO second_tables VALUES
-VALUES (1, "John", 10),
-VALUES (2, "Alex", 3),
-VALUES (3, "Bob", 14),
-VALUES (4, "George", 8);
+INSERT INTO `second_tables` VALUES
+VALUES ('1', "John", '10'),
+VALUES ('2', "Alex", '3'),
+VALUES ('3', "Bob", '14'),
+VALUES ('4', "George", '8');
