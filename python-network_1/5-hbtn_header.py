d5ec18e7dada5bed451de7b4e3c62c48fc62094e
Ante-237 <akumawahanye@gmail.com>
2022-08-03 10:57:34 +0200

    init response header value #1

---
+++ python-network_1/5-hbtn_header.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" send a request and 
+getting a particular header
+"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    r = requests.get(sys.argv[1])
+    print(r.headers['X-Request-Id'])
