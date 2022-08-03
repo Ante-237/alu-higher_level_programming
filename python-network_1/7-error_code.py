eba0e778cb2c5534af3f011b0761e11837057f3d
Ante-237 <akumawahanye@gmail.com>
2022-08-03 11:56:17 +0200

    pep8 fixes

--- python-network_1/7-error_code.py
+++ python-network_1/7-error_code.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" module sends request 
+""" module sends request
     and gets response with specific
     status code
 """
@@ -7,7 +7,9 @@ import requests
 import sys
 
 
-if __name__ ==  "__main__":
+if __name__ == "__main__":
     response = requests.get(sys.argv[1])
     if response.status_code >= 400:
-        print("Error code:{}".format(response.status_code))
+        print("Error code: {}".format(response.status_code))
+    else:
+        print("Regular request")
