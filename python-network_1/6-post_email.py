180638e9746e555bdb64121a7e0f21ee8efdc3b9
Ante-237 <akumawahanye@gmail.com>
2022-08-03 11:33:41 +0200

    init post email

---
+++ python-network_1/6-post_email.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" requesting posting to
+    a web link """
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    # create dictionary of data to post
+    data = {'email', sys.argv[2]}
+    res = requests.post(sys.argv[1], json = data)
