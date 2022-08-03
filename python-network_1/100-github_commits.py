082fa341b575fe02739966289d40a21833adb760
Ante-237 <akumawahanye@gmail.com>
2022-08-03 13:47:25 +0200

    sure

--- python-network_1/100-github_commits.py
+++ python-network_1/100-github_commits.py
@@ -7,7 +7,7 @@ import requests
 
 if __name__ == "__main__":
     res = requests.get('https://api.github.com/repos/{}/{}/commits'
-                       .format(sys.argv[1], sys.argv[2])).json()
+                       .format(sys.argv[2], sys.argv[1])).json()
     count = 0
     for commit in res:
         name = commit.get("commit").get("author").get("name")
