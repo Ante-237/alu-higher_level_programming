43448b227460f401fc1c9073cb5fe5c9e537c8ef
Ante-237 <akumawahanye@gmail.com>
2022-08-04 23:38:13 +0200

    name fix

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,5 +1,5 @@
 -- number of shows by genre
-SELECT tv_genres.name AS genre, COUNT(*) AS number_shows
+SELECT tv_genres.name AS genre, COUNT(*) AS number_of_shows
 FROM tv_show_genres
 INNER JOIN tv_genres 
 ON tv_show_genres.genre_id = tv_genres.id
