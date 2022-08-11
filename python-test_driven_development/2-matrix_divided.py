581e63faf52987d73a498d54fa66134ef802566b
anTe <77624934+Ante-237@users.noreply.github.com>
2022-08-11 12:37:28 -0700

    fix to control flow

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -6,24 +6,26 @@ of a matrix
 
 
 def matrix_divided(matrix, div):
-    """ matrix divide function """
-    for temp_one in matrix:
-        for temp_two in temp_one:
-            if not isinstance(temp_two, (int, float)):
-                raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
-    
-
-    tempCount = -1
-    for row in matrix:
-        if tempCount != len(row) and tempCount != -1:
-            raise TypeError("Each row of the matrix must have the same size")
-        tempCount = len(row)
 
 
     if not isinstance(div, (int, float)):
         raise TypeError("div must be a number")
     if div == 0:
         raise ZeroDivisionError("division by zero")
+        
+        
+    tempCount = -1
+    for row in matrix:
+        if tempCount != len(row) and tempCount != -1:
+            raise TypeError("Each row of the matrix must have the same size")
+        tempCount = len(row)
+       
+    
+     """ matrix divide function """
+    for temp_one in matrix:
+        for temp_two in temp_one:
+            if not isinstance(temp_two, (int, float)):
+                raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
 
     new_matrix = []
     for temp_one in matrix:
