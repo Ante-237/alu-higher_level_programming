0565c54a3208c2c1109b324491053d64f71c573f
Ante-237 <akumawahanye@gmail.com>
2022-08-15 15:47:07 +0200

    test indentation

---
+++ python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+module for text indentation
+"""
+
+
+def text_indentation(text):
+    """ just the function 
+    here """
+    
+    if type(text) is not str:
+        raise TypeError("text must be a string")
+
+    trigger = 0
+    for i in text:
+        if i in ['.','?',':']:
+            print(i)
+            trigger = 1
+            continue
+        if trigger == 1:
+            trigger = 0
+            continue
+        else:
+            print(i, end="")
