344e916b7022ff31562697db59c1b9a794122f25
Ante-237 <akumawahanye@gmail.com>
2022-08-15 18:40:44 +0200

    init base class

---
+++ python-almost_a_circle/models/base.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+just module documentation
+"""
+
+
+class Base:
+
+    def __init__(self, id=None):
+        self.__nb_objects = 0
+        if id is not None:
+            self.id = id
+        else:
+            self.__nb_objects += 1
+            self.id = self.__nb_objects
