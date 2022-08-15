e06167821266bc7261e67bf27c94404fa332fdf1
Ante-237 <akumawahanye@gmail.com>
2022-08-15 16:11:14 +0200

    fixing sure

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -11,16 +11,10 @@ def text_indentation(text):
     if type(text) is not str:
         raise TypeError("text must be a string")
 
-    trigger = 0
-    for i in text:
-        if i in ['.', '?', ':']:
-            print(i)
-            print()
-            trigger = 1
-            continue
-        if trigger == 1:
-            if i is " ":
-                trigger = 0
-                continue
-        else:
-            print(i, end="")
+    line = ""
+    for c in text:
+        line += c
+        if c in ['.', '?', ':']:
+            print((line + '\n').lstrip(' '))
+            line = ""
+    print(line.lstrip(' '), end="")
