1cc268e85b452959875551602d0a9a782e856e3c
Ante-237 <akumawahanye@gmail.com>
2022-08-15 22:48:50 +0200

    sure length fix

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -81,6 +81,10 @@ class Rectangle(Base):
 
     def __str__(self):
         """ the string representation """
+        d = self.id
+        a = self.__x
+        b = self.__y
+        c = self.__width
+        e = self.__height
         return ("[Rectangle] ({}) {:d}/{:d} - {:d}/{:d}".
-                format(self.id, self.__x, self.__y, self.__width,
-                    self.__height))
+                format(d, a, b, c, e))
