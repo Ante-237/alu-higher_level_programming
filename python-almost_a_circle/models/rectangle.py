89c79f0572136ec1a1d0c5377d972616a12af438
Ante-237 <akumawahanye@gmail.com>
2022-08-15 23:38:40 +0200

    another tail :)

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -76,7 +76,7 @@ class Rectangle(Base):
 
     def display(self):
         """ graphic representation """
-        print('\n' * self__y, end='')
+        print('\n' * self.__y, end='')
         for i in range(self.__height):
             print(' ' * self.__x + '#' * self.__width)
 
