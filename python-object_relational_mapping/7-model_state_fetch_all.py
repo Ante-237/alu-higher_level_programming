245ba3513574ef02a46f5258d1abd0c8b64711cd
Ante-237 <akumawahanye@gmail.com>
2022-08-18 17:18:45 +0200

    init all states via sqlalchemy

---
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" module gets all states """
+
+if __name__ == "__main__":
+    from model_state import Base, State
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import sessionmaker
+    from sys import argv
+
+    # create engine that binds to local port sql
+    engine = create_engine('mysql+mysldb://{}:{}@localhost/{}'.
+                            format(argv[1], argv[2], argv[3]),
+                            pool_pre_ping=True)
+    # create session to interact with engine and run queries
+    session = sessionmaker(bind=engine)
+    # handle engine creation from base
+    Base.metadata.create_all(engine)
+    for i in session().query(State).order_by(State.id):
+        print("{}: {}".format(i.id, i.name))
+    # close the session
+    session().close()
