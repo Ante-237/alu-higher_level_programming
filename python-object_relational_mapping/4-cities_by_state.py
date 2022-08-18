c94b199a0dc1fe2152fa2a3bb30c9095fe5f0e11
Ante-237 <akumawahanye@gmail.com>
2022-08-18 15:59:05 +0200

    fix trailing whitespace

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
     # port and host are default local and 3306
     db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
     cur = db.cursor()
-    # link both tables and get base on state_id 
+    # link both tables and get base on state_id
     cur.execute("""SELECT cities.id, cities.name, states.name FROM cities\
     JOIN states ON cities.state_id = states.id ORDER BY cities.id ASC""")
     result = cur.fetchall()
