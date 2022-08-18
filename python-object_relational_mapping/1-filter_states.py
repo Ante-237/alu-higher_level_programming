966dc910c7709f4cdefc20d331b009373a74803b
Ante-237 <akumawahanye@gmail.com>
2022-08-18 15:05:07 +0200

    init filter states

---
+++ python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" module list states
+from database"""
+
+if __name__ == "__main__":
+    import MySQLdb
+    from sys import argv
+    # port and host are default local and 3306
+    db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
+    cur = db.cursor()
+    cur.execute("""SELECT * FROM states ORDER BY states.id ASC""")
+    result = cur.fetchall()
+    # display just last two
+    print(result[3])
+    print(result[4])
+    # close cursor and db
+    cur.close()
+    db.close()
