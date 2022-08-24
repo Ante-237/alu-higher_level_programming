11d3cb69152883d5bce5d007d178a6e7f23d6521
anTe <77624934+Ante-237@users.noreply.github.com>
2022-08-24 02:55:17 -0700

    Update relationship_state.py

--- python-object_relational_mapping/relationship_state.py
+++ python-object_relational_mapping/relationship_state.py
@@ -1,17 +1,21 @@
 #!/usr/bin/python3
-""" model state database declaration """
+"""
+class definition of a State and an instance Base = declarative_base()
+"""
+
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
-
+from sqlalchemy.orm import relationship
 
 Base = declarative_base()
 
 
 class State(Base):
-    """ state class table """
-    # declare table name
+    """
+    State class:
+    """
     __tablename__ = 'states'
-    # define colum names and characteristics
-    id = Column(Integer, primary_key=True, nullable=False)
+    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
     name = Column(String(128), nullable=False)
-    cities = relationship("City", cascade="all, delete", backref="state")
+    cities = relationship('City', cascade='save-update, merge, delete',
+                          backref='state')
