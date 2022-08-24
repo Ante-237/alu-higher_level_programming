8d12570584616a568b083e5e709192cdac0e404c
anTe <77624934+Ante-237@users.noreply.github.com>
2022-08-24 02:54:28 -0700

    Update relationship_city.py

--- python-object_relational_mapping/relationship_city.py
+++ python-object_relational_mapping/relationship_city.py
@@ -1,15 +1,19 @@
 #!/usr/bin/python3
-"""model city module"""
-from model_state import Base, State
-from sqlalchemy import Column, Integer, ForeignKey, String
-from sqlalchemy.orm import relationship
+"""
+class definition of a State and an instance Base = declarative_base()
+"""
+
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+from relationship_state import Base
 
 
 class City(Base):
     """
-    City class inherits from State
+    City class:
+    can’t be null and is a foreign key to states.id
     """
     __tablename__ = 'cities'
-    id = Column('id', Integer, primary_key=True, nullable=False)
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     name = Column(String(128), nullable=False)
-    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
+    state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
