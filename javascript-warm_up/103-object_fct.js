8e3e055ac142033d86295184fcc8fd90dad05a18
Ante-237 <akumawahanye@gmail.com>
2022-10-02 13:36:50 +0200

    changes fix

--- javascript-warm_up/103-object_fct.js
+++ javascript-warm_up/103-object_fct.js
@@ -5,7 +5,7 @@ const myObject = {
 };
 console.log(myObject);
 
-myObject.prototype.incr = function () { myObject.value = myObject.value + 1; };
+myObject.incr = function () { myObject.value = myObject.value + 1; };
 
 myObject.incr();
 console.log(myObject);
