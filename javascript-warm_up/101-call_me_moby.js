5b30f5bdce978962b4ac89028f54ffc9bf687189
Ante-237 <akumawahanye@gmail.com>
2022-10-02 12:24:32 +0200

    exports ==

--- javascript-warm_up/101-call_me_moby.js
+++ javascript-warm_up/101-call_me_moby.js
@@ -1,7 +1,6 @@
-#!/usr/bin/node
-export * from '101-call_me_moby.js';
-function callMeMoby (x, theFunction) {
+#!/usr/bin/node;
+exports.callMeMoby = function (x, theFunction) {
   for (let i = 0; i < x; i++) {
     theFunction();
   }
-}
+};
