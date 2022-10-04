16ba7c528e6904e3dcf5d3aa566dc148d4c7f528
Ante-237 <akumawahanye@gmail.com>
2022-10-04 13:40:50 +0200

    little change

--- javascript_objects_scopes_closures/0-main.js
+++ javascript_objects_scopes_closures/0-main.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const Rectangle = require('./2-rectangle');
 
-const r1 = new Rectangle(2, 2);
+const r1 = new Rectangle(2);
 console.log(r1);
 console.log(r1.width);
 console.log(r1.height);
--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -4,8 +4,9 @@ module.exports = class Rectangle {
   constructor (w, h) {
     this.width = w;
     this.height = h;
-    if (this.width <= 0 || this.height <= 0) {
-      return {};
+    if (this.width <= 0 || this.height <= 0 || (!this.width) || (!this.height)) {
+      this.output = {};
+      return this.output;
     }
   }
 };
