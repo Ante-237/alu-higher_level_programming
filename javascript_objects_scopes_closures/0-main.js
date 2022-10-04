2b9a8e311c1d02d1b50680df8af2ca2b678da595
Ante-237 <akumawahanye@gmail.com>
2022-10-04 15:52:29 +0200

    reverse us

--- javascript_objects_scopes_closures/0-main.js
+++ javascript_objects_scopes_closures/0-main.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
-const nb = require('./7-occurrences').nbOccurences;
+const nb = require('./8-esrever').esrever;
 
-console.log(nb([1, 2, 3, 4, 5, 3], 3));
+console.log(nb([1, 2, 3, 5, 6]));
