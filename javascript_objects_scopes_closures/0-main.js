44138d0e6951d9caa648b9704524831c32b65f7e
Ante-237 <akumawahanye@gmail.com>
2022-10-04 14:24:30 +0200

    init

--- javascript_objects_scopes_closures/0-main.js
+++ javascript_objects_scopes_closures/0-main.js
@@ -1,7 +1,8 @@
 #!/usr/bin/node
 const Rectangle = require('./2-rectangle');
 
-const r1 = new Rectangle(2, 0);
+const r1 = new Rectangle(5, 5);
 console.log(r1);
+r1.print();
 console.log(r1.width);
 console.log(r1.height);
