3eadf9225ef8a492bc6271164cc8420e92f25eb3
Ante-237 <akumawahanye@gmail.com>
2022-10-04 17:59:44 +0200

    sure fix

--- javascript_objects_scopes_closures/100-map.js
+++ javascript_objects_scopes_closures/100-map.js
@@ -1,9 +1,6 @@
 #!/usr/bin/node
 const l1 = require('./100-data').list;
 /* const me = [2, 4, 5, 6, 7]; */
-/* const map1 = me.map((x, y, l1) => x * l1[y]); */
+const map1 = l1.map((x, y) => x * y);
 console.log(l1);
-const l2 = require('./100-data').list;
-console.log(l2);
-const map1 = l1.map(x => x * x);
 console.log(map1);
