e52601d46b6b9e2f1ba23fe8d1b848ec6c08bd0e
Ante-237 <akumawahanye@gmail.com>
2022-10-04 14:05:31 +0200

    empty object

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -2,11 +2,10 @@
 
 module.exports = class Rectangle {
   constructor (w, h) {
+    if (this.width <= 0 || this.height <= 0 || (!this.width) || (!this.height)) {
+      return this;
+    }
     this.width = w;
     this.height = h;
-    if (this.width <= 0 || this.height <= 0 || (!this.width) || (!this.height)) {
-      this.output = {};
-      return this.output;
-    }
   }
 };
