e138a27a83a0e424a45f7faef7e186516093a3b9
Ante-237 <akumawahanye@gmail.com>
2022-10-04 14:39:59 +0200

    double rotate value

--- javascript_objects_scopes_closures/0-main.js
+++ javascript_objects_scopes_closures/0-main.js
@@ -1,8 +1,10 @@
 #!/usr/bin/node
-const Rectangle = require('./2-rectangle');
+const Rectangle = require('./4-rectangle');
 
 const r1 = new Rectangle(5, 5);
 console.log(r1);
+r1.print();
+r1.double();
 r1.print();
 console.log(r1.width);
 console.log(r1.height);
