82137a7cf921dceadb14e921d21bdba23555075f
Ante-237 <akumawahanye@gmail.com>
2022-10-04 15:41:32 +0200

    occure just

--- javascript_objects_scopes_closures/0-main.js
+++ javascript_objects_scopes_closures/0-main.js
@@ -1,11 +1,4 @@
 #!/usr/bin/node
-const Square = require('./6-square');
+const nb = require('./7-occurrences').nbOccurences;
 
-const r1 = new Square(5);
-r1.charPrint();
-r1.charPrint('C');
-r1.double();
-r1.charPrint('C');
-r1.charPrint();
-console.log(r1.size);
-console.log(r1.size);
+console.log(nb([1, 2, 3, 4, 5, 3], 3));
