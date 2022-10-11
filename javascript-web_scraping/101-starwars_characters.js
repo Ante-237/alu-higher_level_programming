95ba10ad735776fae6f59ba5ca589492a5f74644
Ante-237 <akumawahanye@gmail.com>
2022-10-12 00:00:39 +0200

    request

--- javascript-web_scraping/101-starwars_characters.js
+++ javascript-web_scraping/101-starwars_characters.js
@@ -1,6 +1,16 @@
 #!/usr/bin/node
+/*
 const request = require('request');
 const args = process.argv;
+let dicList = {};
+
+function sleep(milliseconds) {
+  const date = Date.now();
+  let currentDate = null;
+  do {
+    currentDate = Date.now();
+  } while (currentDate - date < milliseconds);
+}
 
 request(`https://swapi-api.hbtn.io/api/films/${args[2]}`, function (error, response, body) {
   if (error) {
@@ -15,8 +25,57 @@ request(`https://swapi-api.hbtn.io/api/films/${args[2]}`, function (error, respo
           console.log(error);
         } else {
           console.log(JSON.parse(body).name);
+          dicList[JSON.parse(body).name] = i;
         }
       });
+        sleep(8000);
+    }
+    console.log(dicList);
+  }
+});
+*/
+
+const request = require('request');
+const { argv, exit } = require('process');
+
+if (argv.length !== 3) {
+  exit(0);
+}
+
+const options = {
+  url: `https://swapi-api.hbtn.io/api/films/${argv[2]}/`,
+  method: 'GET',
+  headers: {
+    'Accept-Charset': 'utf-8'
+  }
+};
+
+function requestStart (optionsUrl, saveValue) {
+  request(optionsUrl, function (err, res, body) {
+    if (err) throw err;
+
+    const characters = JSON.parse(body).characters;
+    const result = {};
+
+    characters.forEach(element => {
+      request(element, function (err, res, _body) {
+        if (err) throw err;
+
+        const name = JSON.parse(_body).name;
+
+        result[element.split('/')[5]] = name;
+        saveValue(result, characters.length);
+      });
+    });
+  });
+}
+
+let i = 0;
+requestStart(options, function (dict, length) {
+  i++;
+  if (i === length) {
+    for (const name in dict) {
+      console.log(dict[name]);
     }
   }
 });
