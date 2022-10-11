64b7f94acc0540865c03aced0d9f04952257c4b2
Ante-237 <akumawahanye@gmail.com>
2022-10-11 21:01:54 +0200

    request tunning

---
+++ javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,23 @@
+#!/usr/bin/node
+const request = require('request');
+
+request('https://swapi-api.hbtn.io/api/films', function (error, response, body) {
+  if (error) {
+    console.log(error);
+  } else {
+    const data = JSON.parse(body);
+    const movieCount = data.count;
+    let finalResult = 0;
+    const comString = 'https://swapi-api.hbtn.io/api/people/18/';
+    for (let i = 0; i < movieCount; i++) {
+      const numChars = data.results[i].characters.length;
+      for (let j = 0; j < numChars; j++) {
+        const comString0 = data.results[i].characters[j];
+        if (comString0 === comString) {
+          finalResult += 1;
+        }
+      }
+    }
+    console.log(finalResult);
+  }
+});
