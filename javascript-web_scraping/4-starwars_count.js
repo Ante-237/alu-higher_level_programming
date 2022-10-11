2d970e4d47c0cdc403319477ba8437c8ae550bbb
Ante-237 <akumawahanye@gmail.com>
2022-10-11 21:22:34 +0200

    sure fix

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -6,14 +6,14 @@ request(process.argv[2], function (error, response, body) {
     console.log(error);
   } else {
     const data = JSON.parse(body);
-    const movieCount = data.count;
+    const movieCount = data.results.length;
     let finalResult = 0;
-    const comString = 'https://swapi-api.hbtn.io/api/people/18/';
+    // const comString = 'https://swapi-api.hbtn.io/api/people/18/';
     for (let i = 0; i < movieCount; i++) {
       const numChars = data.results[i].characters.length;
       for (let j = 0; j < numChars; j++) {
         const comString0 = data.results[i].characters[j];
-        if (comString0 === comString) {
+        if (comString0.slice(-3, -1) === '18') {
           finalResult += 1;
         }
       }
